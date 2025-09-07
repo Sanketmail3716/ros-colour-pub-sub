8514b59d8080095388412bd4377d1a9bd4476be6
Sanketmali3716 <malisanket2509@gmail.com>
2025-09-07 23:48:40 +0530

    change colur

--- src/pack/src/scripts/colour_publisher.py
+++ src/pack/src/scripts/colour_publisher.py
@@ -8,10 +8,10 @@ rate = rospy.Rate(10)  # 10 Hz
 pub = rospy.Publisher('/colour', colour, queue_size=10)
 
 color = colour()
-color.r = 255
+color.r = 10
 color.g = 0
 color.b = 0
-color.a = 255
+color.a = 0
 color.name = "White"
 
 while not rospy.is_shutdown():
